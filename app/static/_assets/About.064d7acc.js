9e159c482040e208e90011fdb0f803651572d517
Remy Zulauff <remy.zulauff@gmail.com>
2020-05-24 19:08:30 +0200

    update

--- app/static/_assets/About.064d7acc.js
+++
@@ -1,1 +0,0 @@
-import{c as a,o as s,a as e}from"./index.66e0e255.js";const n={class:"about"},o=e("h1",null,"This is an about page",-1);const t={render:function(e,t){return s(),a("div",n,[o])}};export default t;
